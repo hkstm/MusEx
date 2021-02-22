0e0875d4cc47c966c968d37757bab112ccf97778
romnnn <roman.dahm@gmx.de>
2021-02-22 23:06:50 +0100

    fix python lint and type errors

--- backend/infovis21/__init__.py
+++ backend/infovis21/__init__.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for favico."""
+from infovis21 import views as _views
 
 __author__ = """group14"""
 __email__ = "group14@noreply.com"
 __version__ = "0.1.0"
 
-from flask import Flask
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-from infovis21 import views
+views = _views
