57a55d4720f59e89f98158f720fe96d93c5efb6c
Rebellion <ankur.anmol199@gmail.com>
2021-03-04 13:12:19 +0530

    add heatmap / fullscreen widget to main screen

--- frontend/src/charts/heatmap/heatmap.tsx
+++ frontend/src/charts/heatmap/heatmap.tsx
@@ -1,17 +1,81 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
+import axios from 'axios';
 import "./heatmap.css";
 
-class Heatmap extends Component {
-  constructor(props: {}) {
+// type Year = {
+//   acousticness: number,
+//   danceability: number,
+//   duration_ms: number,
+//   energy: number,
+//   instrumentalness: number,
+//   liveness: number,
+//   loudness: number,
+//   popularity: number,
+//   speechiness: number,
+//   tempo: number,
+//   valence: number,
+//   year: number
+// }
+
+class Heatmap extends Component<{}, any> {
+  constructor(props: any) {
     super(props);
     this.state = {
-      data: [],
+      x: [],
+      y: [],
+      max: {}, //currentMin
+      min: {}, //currentMax
+      hash: {
+        "acousticness": {"max": 0.996, "min": 0.0},
+        "danceability": {"max": 0.988, "min": 0.0},
+        "duration_ms": {"max": 5338302, "min": 4937},
+        "energy": {"max": 1.0, "min": 0.0},
+        "explicit": {"max": 1, "min": 0},
+        "instrumentalness": {"max": 1.0, "min": 0.0},
+        "key": {"max": 11, "min": 0},
+        "liveness": {"max": 1.0, "min": 0.0},
+        "loudness": {"max": 3.855, "min": -60.0},
+        "mode": {"max": 1, "min": 0},
+        "popularity": {"max": 100, "min": 0},
+        "speechiness": {"max": 0.971, "min": 0.0},
+        "tempo": {"max": 243.507, "min": 0.0},
+        "valence": {"max": 1.0, "min": 0.0},
+        "year": {"max": 2021, "min": 1920},
+      }
     };
   }
 
   componentDidMount() {
-    setTimeout(this.d3heatmap, 0)
+    // setTimeout(this.d3heatmap, 0);
+    axios.get(`http://localhost:5000/years?limit=10`).then((res) => {
+      this.setState({ data: res.data.data });
+      var x:number[] = [], y:any[] = [];
+      var min: any = this.state.min, max: any = this.state.max;
+      res.data.data.forEach((d:any) => {
+        x.push(d.year);
+        var info = d;
+        delete info['year'];
+        delete info['loudness'];
+        delete info['duration_ms'];
+
+        // Set 2 cells to min/max shade
+        // var maxKey = Object.keys(d)[Math.floor(Math.random() * Object.keys(d).length)];
+        // info[maxKey] = this.state.hash[maxKey].max;
+        // var minKey = Object.keys(d)[Math.floor(Math.random() * Object.keys(d).length)];
+        // info[minKey] = this.state.hash[minKey].min;
+
+        Object.keys(d).forEach((k:any) => {
+          if (!max[k] || max[k] < d[k])
+            max[k] = d[k]          
+          if (!min[k] || min[k] > d[k])
+            min[k] = d[k]
+        });
+        y.push(info);
+      })
+      this.setState({x, y, min, max})
+    });
+
   }
 
   d3heatmap() {
@@ -87,10 +151,13 @@ class Heatmap extends Component {
     // add the squares
     svg.selectAll()
       .data(data, function(d:any) { return d.t+':'+d.n;})
+      // .data(data, function(d:any, i:any, j: any) { debugger;return d[0].t+','+d[0].n;})
       .enter()
       .append("rect")
         .attr("x", function(d:any) { return x(d.t) })//d.group
         .attr("y", function(d:any) { return y(d.n) })//d.variable
+        // .attr("x", function(d:any, i:any, j:any) { return x(d[i].t) })//d.group
+        // .attr("y", function(d:any, i:any, j: any) { return y(d[i].n) })//d.variable
         .attr("width", x.bandwidth() )
         .attr("height", y.bandwidth() )
         .style("fill", "white" )
@@ -101,9 +168,48 @@ class Heatmap extends Component {
       .transition() // and apply changes to all of them
         .duration(500)
         .style("fill", function(d) { return myColor(d.value)} )
+        // .style("fill", function(d:any,i:any) { return myColor(d[i].value)} )
+  }
+
+  getShade(key:string, val:number) {
+    // var min = this.state.min[key] / 1.2, max = this.state.max[key] * 1.2;
+    var min = this.state.hash[key].min, max = this.state.hash[key].max;
+    var range = max - min;
+    var shade = 255 * (val - min) / (range)
+    return `rgb(${(255) + ',' + (255-shade) + ',' + (255-shade)})`  
+  }
+  
+  draw() {
+    return (
+      <div className="heatmap-grid">
+        {Object.keys(this.state.y[0] || {}).map( (key:string) => 
+          <div className='heatmap-row'>
+            <div className="heatmap-tick-y">
+              {key}
+              <div className="y-tooltip"> {key} Range[{this.state.hash[key].min},{this.state.hash[key].max}]</div>
+            </div>
+            {this.state.x.map( (year:any, i: number) => 
+              <div className='heatmap-row'>
+                <div className="heatmap-cell" style={{background: this.getShade(key, this.state.y[i][key])}}>
+                  <div className="heatmap-cell-tooltip"> {`${key} in ${year} is ${this.state.y[i][key].toFixed(3)}`}</div>
+                </div> 
+              </div>
+              )}
+          </div>
+        )}
+        <div className='heatmap-row'>
+          <div className="heatmap-tick-y"/>
+          {this.state.x.map( (year:any, i: number) => 
+              <div className="heatmap-tick"> {year} </div>
+          )}
+        </div>
+    </div>
+)
   }
 
   render() {
+    debugger;
+    return <div>{this.draw()}</div>;
     return (
         <div className="heatmap-container">
           <svg id="heatmap-svg"></svg>
---
+++ frontend/src/charts/heatmap/observablehq/index.js
@@ -0,0 +1,1 @@
+export {default} from "./90bab46f95409e6e@124.js";
