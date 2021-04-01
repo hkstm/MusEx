7a2ecde4e39d61d408ad31cdcaea110f88366011
ankur1812 <ankur.anmol1992@gmail.com>
2021-04-01 19:30:23 +0530

    hm code refactoring

--- frontend/src/charts/musicheatmap/heatmap.tsx
+++ frontend/src/charts/musicheatmap/heatmap.tsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import * as d3 from "d3";
 import axios from "axios";
 import Slider from "../../components/slider/Slider";
-import "./heatmap.css";
+import "./heatmap.sass";
+import "./heatmap-animate.css";
 
 // type Year = {
 //   acousticness: number,
@@ -51,7 +52,6 @@ class MusicHeatmap extends Component<{apiVersion: string}, any> {
   }
 
   getData() {
-    // setTimeout(this.d3heatmap, 0);
     axios.get(`http://localhost:5000/${this.props.apiVersion}/years?limit=200`).then((res)=>{
       this.setState({data: res.data.data}, ()=> {
         this.setAxes(res.data.data, 40);
@@ -77,140 +77,7 @@ class MusicHeatmap extends Component<{apiVersion: string}, any> {
       y.push(info);
     })
     const heatmapGrid = document.querySelector('.heatmap-grid');
-
-    // let cellSize = Math.floor(heatmapGrid ? heatmapGrid.clientWidth / (yearsLimit + 5) : 25 )
-    // if (cellSize > 40 ) cellSize = 40;
-    // console.log('grid ', heatmapGrid?.clientWidth, ' limit ', yearsLimit, ' cellsize ', cellSize)
-
-    // this.setState({x: [], y: [], min, max, yearsLimit}, ()=> {
-      this.setState({x, y, min, max, yearsLimit});
-    // });
-  }
-
-  d3heatmap() {
-    var margin = { top: 60, right: 60, bottom: 60, left: 60 },
-      width = 480 - margin.left - margin.right,
-      height = 480 - margin.top - margin.bottom;
-
-    // append the svg object to the body of the page
-    var svg = d3
-      .select("#heatmap-svg")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // Labels of row and columns
-    var myGroups = [
-      "Country",
-      "Rock",
-      "Metal",
-      "Hip hop",
-      "Pop",
-      "Jazz",
-      "Rap",
-      "R&B",
-      "Alternative Rock",
-      "Pop Rock",
-    ];
-    var myVars = [
-      "2001",
-      "2002",
-      "2003",
-      "2004",
-      "2005",
-      "2006",
-      "2007",
-      "2008",
-      "2009",
-      "2010",
-    ];
-
-    // Build X scales and axis:
-    var x: any = d3
-      .scaleBand()
-      .range([0, width])
-      .domain(myGroups)
-      .padding(0.01);
-    svg
-      .append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-    var y: any = d3.scaleBand().range([height, 0]).domain(myVars).padding(0.01);
-    svg.append("g").call(d3.axisLeft(y));
-
-    var myColor = function (n: number) {
-      var shade = n * 2.55;
-      return `rgb(${255 - shade}, ${255 - shade}, ${255})`;
-    };
-
-    const buildData = () => {
-      return myVars.reduce((acc, d) => {
-        const o = myGroups.map((t) => ({
-          t: t,
-          n: d,
-          value: Math.floor(Math.random() * 100),
-        }));
-        acc.push(o);
-        return acc;
-      }, [] as any[]);
-    };
-
-    const data = buildData();
-
-    var tooltip = d3
-      .select(".heatmap-container")
-      .append("div")
-      .style("opacity", 0)
-      .attr("class", "heatmap-tooltip");
-
-    var mouseover = function (d: any) {
-      tooltip.style("opacity", 1);
-    };
-    var mousemove = function (d: any, data: any) {
-      tooltip
-        .html(data.value + " " + data.t + " records sold in " + data.n)
-        .style("left", d.clientX + 20 + "px")
-        .style("top", d.clientY - 150 + "px");
-      // .style("left", (d3.pointer(this)[0]+70) + "px")
-      // .style("top", (d3.pointer(this)[1]) + "px")
-    };
-    var mouseleave = function (d: any) {
-      tooltip.style("opacity", 0);
-    };
-
-    // add the squares
-    svg
-      .selectAll()
-      .data(data, function (d: any) {
-        return d.t + ":" + d.n;
-      })
-      // .data(data, function(d:any, i:any, j: any) { debugger;return d[0].t+','+d[0].n;})
-      .enter()
-      .append("rect")
-      .attr("x", function (d: any) {
-        return x(d.t);
-      }) //d.group
-      .attr("y", function (d: any) {
-        return y(d.n);
-      }) //d.variable
-      // .attr("x", function(d:any, i:any, j:any) { return x(d[i].t) })//d.group
-      // .attr("y", function(d:any, i:any, j: any) { return y(d[i].n) })//d.variable
-      .attr("width", x.bandwidth())
-      .attr("height", y.bandwidth())
-      .style("fill", "white")
-      // .style("fill", function(d) { return myColor(d.value)} )
-      .on("mouseover", mouseover)
-      .on("mousemove", mousemove)
-      .on("mouseleave", mouseleave)
-      .transition() // and apply changes to all of them
-      .duration(500)
-      .style("fill", function (d) {
-        return myColor(d.value);
-      });
-    // .style("fill", function(d:any,i:any) { return myColor(d[i].value)} )
+    this.setState({x, y, min, max, yearsLimit});
   }
 
   drawLegend() {
@@ -222,26 +89,6 @@ class MusicHeatmap extends Component<{apiVersion: string}, any> {
       </div>
     );
   }
-
-  /*updateXAxis(e: any) {
-    console.log("clicked at ", e.clientX);
-    let yearsCount: number = Math.round((e.clientX - 660) / 40) * 10 || 10;
-    this.setState({ yearsCount });
-  }
-
-  sliderBar() {
-    return (
-      <div className="hm-slider-bar" onClick={this.updateXAxis}>
-        <div
-          className="hm-slider-stick"
-          style={{ left: this.state.yearsCount * 4 + "px" }}
-        />
-        <div className="hm-slider-label">
-          Show last {this.state.yearsCount} years
-        </div>
-      </div>
-    );
-  }*/
 
   getShade(key: string, val: number) {
     // var min = this.state.min[key] / 1.2, max = this.state.max[key] * 1.2;
@@ -267,18 +114,15 @@ class MusicHeatmap extends Component<{apiVersion: string}, any> {
               </div>
             </div>
             {this.state.x.map((year: any, i: number) => (
-              // <div className="heatmap-row">
                 <div
                   className={"heatmap-cell " + "animation-cell-"+ Math.ceil(Math.random()*5)} style={{
                     background: this.getShade(key, this.state.y[i][key]),
                   }}
                 >
                   <div className="heatmap-cell-tooltip">
-                    {" "}
                     {`${key} in ${year} is ${this.state.y[i][key].toFixed(3)}`}
                   </div>
                 </div>
-              // </div>
             ))}
           </div>
         ))}
@@ -301,7 +145,6 @@ class MusicHeatmap extends Component<{apiVersion: string}, any> {
   render() {
     return (
       <div className="heatmap-container">
-        {/* <svg id="heatmap-svg"></svg> */}
         {this.draw()}
         {this.drawLegend()}
         <Slider id="heatmap" min={40} onUpdate={(n:number)=>{this.updateHeatmap(n);}}/>
